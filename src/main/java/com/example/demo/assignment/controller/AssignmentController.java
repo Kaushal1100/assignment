f70032259006f64270430befddcbaffd32079c61
Kaushal <silwal10kaushal@gmail.com>
2025-03-04 10:49:36 -0500

    Initial Commit

---
+++ src/main/java/com/example/demo/assignment/controller/AssignmentController.java
@@ -0,0 +1,19 @@
+package com.example.demo.assignment.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AssignmentController {
+//	http://localhost:8083/swagger-ui/index.html
+	@GetMapping("/")
+	public String currentDateandTime() {
+		LocalDateTime time = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		return time.format(formatter);
+	}
+
+}
